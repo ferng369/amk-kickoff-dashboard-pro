ad4c354c92abd37a8ce50f47aee618eca6e621aa
ferng369 <lyhengkea2004@gmail.com>
2025-07-31 01:50:38 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 10000,
+    strictPort: true,
+  },
+  preview: {
+    port: 10000,
+    host: '0.0.0.0',
+    allowedHosts: ['amk-agenda.onrender.com'],
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
